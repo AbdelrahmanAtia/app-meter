fc275bf56dd51c5802fd8e6ddb1a4e429ba05620
Abdelrahman_Attya <abdelrahmanattya94@gmail.com>
2021-03-05 20:42:13 +0200

    updated to do

--- application/src/main/java/com/javaworld/application/main/AppMeterApplication.java
+++ application/src/main/java/com/javaworld/application/main/AppMeterApplication.java
@@ -49,7 +49,6 @@ import org.w3c.dom.Document;
  * 4- handle CORS in a better way instead of setting headers directly
  *    in the controller..allow specific  origin only not *
  *    
- * 5-  (FE) => change mouse shape to hand when hover on >> mark
  * 6-  (FE) => fill audit Info fields with data from BE
  * 7-  (FE) => add audit entry id to url and retrieve 
  *             audit info based on it.
@@ -62,11 +61,9 @@ import org.w3c.dom.Document;
  * 9- hide all useless elements of the angular template..
  * 11- BE >> add devtools dependency   --->> don't it cause alot of weird memory issues
  * 12- BE >> configure hibernate to use a custom logger
- * 13- BE >> enable pagination
- * 14- BE >> allow searching for audits of a certain user
  * 15- FE >> send audit info details from audit list component to audit-info component
  *           when clicking on >> icon.. and don't retrieve audit details from backend
- * 16- FE >>  add pagination, search by username, filter by response status [success/fail]
+ * 16- FE >> filter by response status [success/fail]
  *     
  * done
  *=======
@@ -74,6 +71,9 @@ import org.w3c.dom.Document;
  * >> BE >> handle case when log file does not has date in it's name
  *           [the same day request]
  * >> BE >> return only first 20 request.
+ * >> FE & BE >> add pagination, search by user name & URL
+ * >> FE >> change mouse shape to hand when hover on >> mark
+
  */
 
 //@EntityScan(basePackages = { "com.javaworld.application" })
